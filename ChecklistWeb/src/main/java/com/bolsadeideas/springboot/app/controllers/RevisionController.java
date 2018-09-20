7cb25b1e78c3d84adbd3e2d97fdfaced0eee888e
jmendosa <jmendosa@LIM-1VR5QN2.usersad.everis.int>
2018-09-20 18:14:37 -0500

    XXXX

--- ChecklistWeb/src/main/java/com/bolsadeideas/springboot/app/controllers/RevisionController.java
+++ ChecklistWeb/src/main/java/com/bolsadeideas/springboot/app/controllers/RevisionController.java
@@ -1,15 +1,23 @@
 package com.bolsadeideas.springboot.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.bolsadeideas.springboot.app.models.service.IElementoService;
 
 @Controller
+@SessionAttributes({"elementos"})
 public class RevisionController {
+	@Autowired
+	private IElementoService elementoService;
 	
 	@RequestMapping(value = { "/revision" })
 	public String init(Model model) {
 		model.addAttribute("cabecera", "Revision");
+		model.addAttribute("elementos", elementoService.findAll());
 		return "/revision/content_revision";
 	}
 	
